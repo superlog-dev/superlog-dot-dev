d1d09d021c6e71f105910812b9a5c4199b383b6d
Yu-Chien Chan <yuchien302@gmail.com>
2022-06-28 22:43:23 -0700

    Extract LandingPageHeader

--- src/component/LandingPageHeader.js
+++ src/component/LandingPageHeader.js
@@ -1,88 +1,59 @@
-import PrimaryDownloadButton from "landing-page/PrimaryDownloadButton";
-import amplitude from "amplitude-js";
-
+// import PrimaryDownloadButton from "landing-page/PrimaryDownloadButton";
+import Link from "next/link";
 export default function LandingPageHeader({ enableDownload }) {
   return (
-    <header className="header-area">
-      <div className="navbar-area">
-        <div className="max-w-screen-lg m-auto px-4">
-          <nav className="navbar navbar-expand-lg">
-            <a className="navbar-brand relative -left-2" href="/">
-              <img src="assets/images/logo.svg" alt="Logo" />
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="toggler-icon"></span>
-              <span className="toggler-icon"></span>
-              <span className="toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-              <ul id="nav" className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a
-                    className="page-scroll"
-                    href="https://discord.gg/KAkXjPX8Yn"
-                    onClick={() => {
-                      amplitude.getInstance().logEvent("header_discord_clicked");
-                    }}
-                    target="_blank"
-                  >
-                    Discord
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="page-scroll"
-                    href="https://heptaplatforms.notion.site/heptaplatforms/Heptabase-Public-Wiki-4dcb425de51c42c28d2fd57d0dbbdcdd"
-                    onClick={() => {
-                      amplitude.getInstance().logEvent("header_wiki_clicked");
-                    }}
-                    target="_blank"
-                  >
-                    Wiki
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="page-scroll"
-                    href="https://twitter.com/Heptabase"
-                    onClick={() => {
-                      amplitude.getInstance().logEvent("header_twitter_clicked");
-                    }}
-                    target="_blank"
-                  >
-                    Twitter
-                  </a>
-                </li>
-              </ul>
-            </div>
-            {enableDownload ? (
-              <div className="hidden lg:block">
-                <PrimaryDownloadButton size={"sm"} />
-              </div>
-            ) : (
-              <div className="navbar-btn d-none d-sm-inline-block">
-                <a
-                  className="main-btn"
-                  data-scroll-nav="0"
-                  href="https://projectmeta.substack.com/p/coming-soon"
-                  rel="nofollow"
-                >
-                  Join the Waitlist
-                </a>
-              </div>
-            )}
-          </nav>
+    <div className="z-20 sticky w-screen bg-0 top-0 backdrop-blur bg-opacity-80 border-bg-1 border-b">
+      <nav className="flex mx-auto items-center h-16 max-w-5xl px-4">
+        <div className="w-full flex items-center">
+          <Link href="/" passHref>
+            <a className="text-gray-85 font-bold text-xl">Superlog</a>
+          </Link>
         </div>
-      </div>
-    </header>
+        <div className="flex space-x-6">
+          <a href="https://medium.com/smallpr" className="text-2-on-1 hover:text-gray-80">
+            Blog
+          </a>
+          <a
+            className="text-gray-85 group flex items-center space-x-2 transition-all duration-300"
+            href="https://join.slack.com/t/smallpr/shared_invite/zt-17hdr0wlv-PJ16YUw6Tz_o6ZYLcrIv~A"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {/* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 60 60"
+                preserveAspectRatio="xMidYMid meet"
+                alt="Slack"
+              >
+                <title>Slack</title>
+                <path
+                  d="M22,12 a6,6 0 1 1 6,-6 v6z M22,16 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"
+                  className={`fill-gray-70 group-hover:fill-[#36C5F0]`}
+                />
+                <path
+                  d="M48,22 a6,6 0 1 1 6,6 h-6z M32,6 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"
+                  className={`fill-gray-70 group-hover:fill-[#2EB67D]`}
+                  fill="#2EB67D"
+                />
+                <path
+                  d="M38,48 a6,6 0 1 1 -6,6 v-6z M54,32 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"
+                  className={`fill-gray-70 group-hover:fill-[#ECB22E]`}
+                  fill="#ECB22E"
+                />
+                <path
+                  d="M12,38 a6,6 0 1 1 -6,-6 h6z M16,38 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"
+                  className={`fill-gray-70 group-hover:fill-[#E01E5A]`}
+                  fill="#E01E5A"
+                />
+              </svg> */}
+            <span className="text-2-on-1 whitespace-nowrap group-hover:text-gray-80">Community</span>
+          </a>
+          <Link href="/about" passHref className="text-2-on-1 hover:text-gray-80 whitespace-nowrap">
+            <a className="text-2-on-1 hover:text-gray-80 whitespace-nowrap">About Us</a>
+          </Link>
+        </div>
+      </nav>
+    </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Head from "next/head";
 // import styles from "../styles/Home.module.css";
+import LandingPageHeader from "component/LandingPageHeader";
 
 export default function Home() {
   return (
@@ -19,59 +20,7 @@ export default function Home() {
         />
       </Head>
 
-      <div className="z-20 sticky w-screen bg-0 top-0 backdrop-blur bg-opacity-80 border-bg-1 border-b">
-        <nav className="flex mx-auto items-center h-16 max-w-5xl px-4">
-          <div className="w-full flex items-center">
-            <a href="/">
-              <span className="text-gray-85 font-bold text-xl">Superlog</span>
-            </a>
-          </div>
-          <div className="flex space-x-6">
-            <a href="https://medium.com/smallpr" className="text-2-on-1 hover:text-gray-80">
-              Blog
-            </a>
-            <a
-              className="text-gray-85 group flex items-center space-x-2 transition-all duration-300"
-              href="https://join.slack.com/t/smallpr/shared_invite/zt-17hdr0wlv-PJ16YUw6Tz_o6ZYLcrIv~A"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 0 60 60"
-                preserveAspectRatio="xMidYMid meet"
-                alt="Slack"
-              >
-                <title>Slack</title>
-                <path
-                  d="M22,12 a6,6 0 1 1 6,-6 v6z M22,16 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"
-                  className={`fill-gray-70 group-hover:fill-[#36C5F0]`}
-                />
-                <path
-                  d="M48,22 a6,6 0 1 1 6,6 h-6z M32,6 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"
-                  className={`fill-gray-70 group-hover:fill-[#2EB67D]`}
-                  fill="#2EB67D"
-                />
-                <path
-                  d="M38,48 a6,6 0 1 1 -6,6 v-6z M54,32 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"
-                  className={`fill-gray-70 group-hover:fill-[#ECB22E]`}
-                  fill="#ECB22E"
-                />
-                <path
-                  d="M12,38 a6,6 0 1 1 -6,-6 h6z M16,38 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"
-                  className={`fill-gray-70 group-hover:fill-[#E01E5A]`}
-                  fill="#E01E5A"
-                />
-              </svg> */}
-              <span className="text-2-on-1 whitespace-nowrap group-hover:text-gray-80">Community</span>
-            </a>
-            <a href="https://medium.com/smallpr" className="text-2-on-1 hover:text-gray-80 whitespace-nowrap">
-              About Us
-            </a>
-          </div>
-        </nav>
-      </div>
+      <LandingPageHeader />
 
       <main className="text-center pt-20 px-4">
         <h1 className={"font-bold text-gray-95 text-4xl lg:text-5xl tracking-tight pb-4"}>
@@ -110,7 +59,7 @@ export default function Home() {
             target={"_blank"}
             rel="noreferrer"
           >
-            Get Early Access
+            Download Superlog for Mac
           </a>
         </div>
 
@@ -164,7 +113,7 @@ export default function Home() {
           </video>
         </div>
       </main>
-      <footer className="h-96"></footer>
+      <footer className="h-96 text-white"> 123</footer>
     </div>
   );
 }
