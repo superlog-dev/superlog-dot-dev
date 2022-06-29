564ca9083df3f89127df419381c5548ebc11c457
Yu-Chien Chan <yuchien302@gmail.com>
2022-06-29 16:36:25 -0700

    Update doc

--- src/component/docs/utils/docs.js
+++ src/component/docs/utils/docs.js
@@ -1,10 +1,10 @@
 const docs = [
-  { path: ["guide"], title: "Superlog Essentials", id: "guide" },
-  { path: ["guide", "step1"], title: "1 - Before you start", id: "s1" },
-  { path: ["guide", "step2"], title: "2 - Commit & Uncommit", id: "s2" },
-  { path: ["guide", "step3"], title: "3 - Submit for code review", id: "s3" },
-  { path: ["guide", "step4"], title: "4 - Addressing feedback", id: "s4" },
-  { path: ["guide", "step5"], title: "5 - Shpi it!", id: "s5" },
+  { path: ["essentials"], title: "Superlog Essentials", id: "s0" },
+  { path: ["essentials", "step1"], title: "1 - Before you code", id: "s1" },
+  { path: ["essentials", "step2"], title: "2 - Commit & Uncommit", id: "s2" },
+  { path: ["essentials", "step3"], title: "3 - Submit for code review", id: "s3" },
+  { path: ["essentials", "step4"], title: "4 - Addressing feedback", id: "s4" },
+  { path: ["essentials", "step5"], title: "5 - Shpi it!", id: "s5" },
   { path: ["FAQ"], title: "FAQ", id: "faq" },
   { path: ["About us"], title: "About us", id: "about" },
 ];
--- src/pages/docs/[...docPath].js
+++ src/pages/docs/[...docPath].js
@@ -1,6 +1,7 @@
 import DocLayout from "component/docs/DocLayout";
 import docs from "component/docs/utils/docs";
 import _ from "lodash";
+import Essential from "docs/guide.mdx";
 import Step1 from "docs/step1-before.mdx";
 import Step2 from "docs/step2-commit.mdx";
 import Step3 from "docs/step3-prepare.mdx";
@@ -8,6 +9,8 @@ import Step4 from "docs/step4-amend.mdx";
 
 function DocContent({ doc }) {
   switch (doc.id) {
+    case "s0":
+      return <Essential />;
     case "s1":
       return <Step1 />;
     case "s2":
