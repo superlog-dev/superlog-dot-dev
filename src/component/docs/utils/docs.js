962aed8792c32378262c7fd615c09a71bbeac744
Yu-Chien Chan <yuchien302@gmail.com>
2022-07-21 16:51:02 -0700

    Update docs

--- src/component/docs/utils/docs.js
+++ src/component/docs/utils/docs.js
@@ -2,9 +2,9 @@ const docs = [
   { path: ["essentials"], title: "Superlog Essentials", id: "s0" },
   { path: ["essentials", "1-before-coding"], title: "1. Before coding", id: "s1" },
   { path: ["essentials", "2-after-coding"], title: "2. After coding", id: "s2" },
-  { path: ["essentials", "3-create-pr"], title: "3. Create PR", id: "s3" },
-  { path: ["essentials", "4-iterate-on-feedback"], title: "4. Iterate on feedback", id: "s4" },
-  { path: ["essentials", "5-merge-pr"], title: "5. Merge PR", id: "s5" },
+  { path: ["essentials", "3-creating-pr"], title: "3. Creating PR", id: "s3" },
+  { path: ["essentials", "4-iterating-on-feedback"], title: "4. Iterating on feedback", id: "s4" },
+  { path: ["essentials", "5-merging-pr"], title: "5. Merging PR", id: "s5" },
 ];
 
 export default docs;
